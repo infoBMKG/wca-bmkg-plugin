31a7e5b4aa37506b47f0655500f4e3c731dfac17
Raksaka Indra <raksakaindra@gmail.com>
2024-07-30 12:41:22 +0700

    Add excerpt in single post.

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # WP-BMKG Custom API (`wca-bmkg-plugin`)
+
 Plugin WordPress untuk custom REST API endopoint untuk post, page, dan search. Plugin ini melakukan custom endpoint URL pada response REST API berikut:
+
 1. List post
 2. Single post
 3. Search post
 4. Single page
 
 ## URL Endpoint
+
 1. List post: `{site}/wp-json/wca/v1/posts/{category}/{post_per_page}/{offset}`
 2. Single post: `{site}/wp-json/wca/v1/posts/{slug}`
 3. Search post: `{site}/wp-json/wca/v1/search/{category}/{search}/{offset}`
 4. Single page: `{site}/wp-json/wca/v1/pages/{slug}`
 
 Keterangan:
+
 - `category: int`
 - `post_per_page: int`
 - `offiset: int`
@@ -19,9 +23,12 @@ Keterangan:
 - `search: string`
 
 ## Referensi
+
 https://developer.wordpress.org/rest-api/extending-the-rest-api/adding-custom-endpoints/
 
 ## Changelog
+
 - `v1.0`: 12 Mei 2024
 - `v1.1`: 22 Mei 2024
 - `v1.2`: 19 Juni 2024
+- `v1.3`: 30 Juli 2024
--- wca-bmkg.php
+++ wca-bmkg.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WP-BMKG Custom API
  * Plugin URI:        https://github.com/infoBMKG/wca-bmkg-plugin/
  * Description:       WordPress Custom REST API for BMKG Content
- * Version:           1.2
+ * Version:           1.3
  * Requires at least: 5.6
  * Requires PHP:      7.2
  * Author:            Raksaka Indra
@@ -13,181 +13,8 @@
  * Update URI:        https://github.com/infoBMKG/wca-bmkg-plugin/
  */
 
-// List Posts Func
-function wca_list_posts($param) {
-	$args = [
-		'post_type' => 'post',
-		'post_status' => 'publish',
-		'cat' => $param['cat'],
-		'posts_per_page' => $param['perpage'],
-		'offset' => $param['offset']
-	];
-	
-	$query = new WP_Query($args);
-	$posts = $query->get_posts();
-
-	$data = [];
-	$i = 0;
-
-	foreach($posts as $post) {
-		$data[$i]['total'] = $query->found_posts;
-		$data[$i]['date'] = $post->post_date;
-		$data[$i]['title'] = apply_filters('the_title', $post->post_title);
-		$data[$i]['slug'] = $post->post_name;
-		$data[$i]['excerpt'] = apply_filters('the_excerpt', $post->post_excerpt);
-		$data[$i]['featured_image']['thumbnail'] = get_the_post_thumbnail_url($post->ID, 'thumbnail');
-		$data[$i]['featured_image']['medium'] = get_the_post_thumbnail_url($post->ID, 'medium');
-		$data[$i]['featured_image']['large'] = get_the_post_thumbnail_url($post->ID, 'large');
-		$data[$i]['featured_image']['full'] = get_the_post_thumbnail_url($post->ID, 'full');
-		$i++;
-	}
-	
-	if ( empty( $posts ) ) {
-		return new WP_Error( 'post_not_found', 'Post not found.', array('status' => 404));
-	}
-
-	return $data;
-}
-
-// Single Post Func
-function wca_posts( $slug ) {
-	$args = [
-		'name' => $slug['slug'],
-		'post_type' => 'post'
-	];
-
-	$query = new WP_Query($args);
-	$post = $query->get_posts();
-
-	$data['date'] = $post[0]->post_date;
-	$data['title'] = apply_filters('the_title', $post[0]->post_title);
-	$data['author'] = get_the_author_meta('display_name', $post[0]->post_author);
-	$data['content'] = apply_filters('the_content', $post[0]->post_content);
-	$data['featured_image']['medium'] = get_the_post_thumbnail_url($post[0]->ID, 'medium');
-	$data['featured_image']['large'] = get_the_post_thumbnail_url($post[0]->ID, 'large');
-	$data['featured_image']['full'] = get_the_post_thumbnail_url($post[0]->ID, 'full');
-	
-	if ( empty( $post ) ) {
-		return new WP_Error( 'post_not_found', 'Post not found.', array('status' => 404));
-	}
-
-	return $data;
-}
-
-// Search Posts Func
-function wca_search_posts($query) {
-	$args = [
-		'posts_per_page' => 12,
-		'post_type' => 'post',
-		'post_status' => 'publish',
-		'cat' => $query['cat'],
-		's' => $query['search'],
-		'offset' => $query['offset']
-	];
-	
-	$query = new WP_Query($args);
-	$posts = $query->get_posts();
-
-	$data = [];
-	$i = 0;
-
-	foreach($posts as $post) {
-		$data[$i]['total'] = $query->found_posts;
-		$data[$i]['date'] = $post->post_date;
-		$data[$i]['title'] = apply_filters('the_title', $post->post_title);
-		$data[$i]['slug'] = $post->post_name;
-		$data[$i]['excerpt'] = apply_filters('the_excerpt', $post->post_excerpt);
-		$data[$i]['featured_image']['thumbnail'] = get_the_post_thumbnail_url($post->ID, 'thumbnail');
-		$data[$i]['featured_image']['medium'] = get_the_post_thumbnail_url($post->ID, 'medium');
-		$i++;
-	}
-	
-	if ( empty( $posts ) ) {
-		return new WP_Error( 'post_not_found', 'Post not found.', array('status' => 404));
-	}
-
-	return $data;
-}
-
-// Single Page Func
-function wca_pages( $slug ) {
-	$args = [
-		'name' => $slug['slug'],
-		'post_type' => 'page'
-	];
-
-	$query = new WP_Query($args);
-	$post = $query->get_posts();
-
-	$data['date'] = $post[0]->post_date;
-	$data['title'] = apply_filters('the_title', $post[0]->post_title);
-	$data['content'] = apply_filters('the_content', $post[0]->post_content);
-	
-	if ( empty( $post ) ) {
-		return new WP_Error( 'page_not_found', 'Page not found.', array('status' => 404));
-	}
-
-	return $data;
-}
-
-// Add Custom Endpoint
-add_action('rest_api_init', function() {
-	register_rest_route('wca/v1', 'posts/(?P<cat>\d+)/(?P<perpage>\d+)/(?P<offset>\d+)', array(
-		'methods' => 'GET',
-		'callback' => 'wca_list_posts',
-		// 'args' => array(
-		// 	'cat' => array(
-		// 		'validate_callback' => is_numeric
-		// 	),
-		// 	'perpage' => array(
-		// 		'validate_callback' => is_numeric
-		// 	),
-		// 	'offset' => array(
-		// 		'validate_callback' => is_numeric
-		// 	),
-		// ),
-	));
-
-	register_rest_route( 'wca/v1', 'posts/(?P<slug>[a-zA-Z0-9-]+)', array(
-		'methods' => 'GET',
-		'callback' => 'wca_posts',
-		// 'args' => array(
-		// 	'slug' => array(
-		// 		'validate_callback' => is_string
-		// 	),
-		// ),
-	));
-	
-	register_rest_route( 'wca/v1', 'search/(?P<cat>\d+)/(?P<search>.+)/(?P<offset>\d+)', array(
-		'methods' => 'GET',
-		'callback' => 'wca_search_posts',
-		// 'args' => array(
-		// 	'cat' => array(
-		// 		'validate_callback' => is_numeric
-		// 	),
-		// 	'search' => array(
-		// 		'validate_callback' => is_string
-		// 	),
-		// 	'offset' => array(
-		// 		'validate_callback' => is_numeric
-		// 	),
-		// ),
-	));
-	
-	register_rest_route( 'wca/v1', 'pages/(?P<slug>[a-zA-Z0-9-]+)', array(
-		'methods' => 'GET',
-		'callback' => 'wca_pages',
-		// 'args' => array(
-		// 	'slug' => array(
-		// 		'validate_callback' => is_string
-		// 	),
-		// ),
-	));
-});
-
 // Plugin updater
 // Thanks to https://github.com/rudrastyh/misha-update-checker
-
 defined( 'ABSPATH' ) || exit;
 
 if( ! class_exists( 'wcaBmkgUpdate' ) ) {
@@ -204,7 +31,7 @@ if( ! class_exists( 'wcaBmkgUpdate' ) ) {
 
 			$this->plugin_slug = plugin_basename( __DIR__ );
 			$this->plugin_basename_file = plugin_basename( __FILE__ );
-			$this->version = '1.2';
+			$this->version = '1.3';
 			$this->cache_key = 'wca_bmkg_upd';
 			$this->cache_allowed = false;
 
@@ -372,3 +199,176 @@ if( ! class_exists( 'wcaBmkgUpdate' ) ) {
 	new wcaBmkgUpdate();
 
 }
+
+// List Posts Func
+function wca_list_posts($param) {
+	$args = [
+		'post_type' => 'post',
+		'post_status' => 'publish',
+		'cat' => $param['cat'],
+		'posts_per_page' => $param['perpage'],
+		'offset' => $param['offset']
+	];
+	
+	$query = new WP_Query($args);
+	$posts = $query->get_posts();
+
+	$data = [];
+	$i = 0;
+
+	foreach($posts as $post) {
+		$data[$i]['total'] = $query->found_posts;
+		$data[$i]['date'] = $post->post_date;
+		$data[$i]['title'] = apply_filters('the_title', $post->post_title);
+		$data[$i]['slug'] = $post->post_name;
+		$data[$i]['excerpt'] = apply_filters('the_excerpt', $post->post_excerpt);
+		$data[$i]['featured_image']['thumbnail'] = get_the_post_thumbnail_url($post->ID, 'thumbnail');
+		$data[$i]['featured_image']['medium'] = get_the_post_thumbnail_url($post->ID, 'medium');
+		$data[$i]['featured_image']['large'] = get_the_post_thumbnail_url($post->ID, 'large');
+		$data[$i]['featured_image']['full'] = get_the_post_thumbnail_url($post->ID, 'full');
+		$i++;
+	}
+	
+	if ( empty( $posts ) ) {
+		return new WP_Error( 'post_not_found', 'Post not found.', array('status' => 404));
+	}
+
+	return $data;
+}
+
+// Single Post Func
+function wca_posts( $slug ) {
+	$args = [
+		'name' => $slug['slug'],
+		'post_type' => 'post'
+	];
+
+	$query = new WP_Query($args);
+	$post = $query->get_posts();
+
+	$data['date'] = $post[0]->post_date;
+	$data['title'] = apply_filters('the_title', $post[0]->post_title);
+	$data['author'] = get_the_author_meta('display_name', $post[0]->post_author);
+	$data['content'] = apply_filters('the_content', $post[0]->post_content);
+	$data[$i]['excerpt'] = apply_filters('the_excerpt', $post[0]->post_excerpt);
+	$data['featured_image']['medium'] = get_the_post_thumbnail_url($post[0]->ID, 'medium');
+	$data['featured_image']['large'] = get_the_post_thumbnail_url($post[0]->ID, 'large');
+	$data['featured_image']['full'] = get_the_post_thumbnail_url($post[0]->ID, 'full');
+	
+	if ( empty( $post ) ) {
+		return new WP_Error( 'post_not_found', 'Post not found.', array('status' => 404));
+	}
+
+	return $data;
+}
+
+// Search Posts Func
+function wca_search_posts($query) {
+	$args = [
+		'posts_per_page' => 12,
+		'post_type' => 'post',
+		'post_status' => 'publish',
+		'cat' => $query['cat'],
+		's' => $query['search'],
+		'offset' => $query['offset']
+	];
+	
+	$query = new WP_Query($args);
+	$posts = $query->get_posts();
+
+	$data = [];
+	$i = 0;
+
+	foreach($posts as $post) {
+		$data[$i]['total'] = $query->found_posts;
+		$data[$i]['date'] = $post->post_date;
+		$data[$i]['title'] = apply_filters('the_title', $post->post_title);
+		$data[$i]['slug'] = $post->post_name;
+		$data[$i]['excerpt'] = apply_filters('the_excerpt', $post->post_excerpt);
+		$data[$i]['featured_image']['thumbnail'] = get_the_post_thumbnail_url($post->ID, 'thumbnail');
+		$data[$i]['featured_image']['medium'] = get_the_post_thumbnail_url($post->ID, 'medium');
+		$i++;
+	}
+	
+	if ( empty( $posts ) ) {
+		return new WP_Error( 'post_not_found', 'Post not found.', array('status' => 404));
+	}
+
+	return $data;
+}
+
+// Single Page Func
+function wca_pages( $slug ) {
+	$args = [
+		'name' => $slug['slug'],
+		'post_type' => 'page'
+	];
+
+	$query = new WP_Query($args);
+	$post = $query->get_posts();
+
+	$data['date'] = $post[0]->post_date;
+	$data['title'] = apply_filters('the_title', $post[0]->post_title);
+	$data['content'] = apply_filters('the_content', $post[0]->post_content);
+	
+	if ( empty( $post ) ) {
+		return new WP_Error( 'page_not_found', 'Page not found.', array('status' => 404));
+	}
+
+	return $data;
+}
+
+// Add Custom Endpoint
+add_action('rest_api_init', function() {
+	register_rest_route('wca/v1', 'posts/(?P<cat>\d+)/(?P<perpage>\d+)/(?P<offset>\d+)', array(
+		'methods' => 'GET',
+		'callback' => 'wca_list_posts',
+		// 'args' => array(
+		// 	'cat' => array(
+		// 		'validate_callback' => is_numeric
+		// 	),
+		// 	'perpage' => array(
+		// 		'validate_callback' => is_numeric
+		// 	),
+		// 	'offset' => array(
+		// 		'validate_callback' => is_numeric
+		// 	),
+		// ),
+	));
+
+	register_rest_route( 'wca/v1', 'posts/(?P<slug>[a-zA-Z0-9-]+)', array(
+		'methods' => 'GET',
+		'callback' => 'wca_posts',
+		// 'args' => array(
+		// 	'slug' => array(
+		// 		'validate_callback' => is_string
+		// 	),
+		// ),
+	));
+	
+	register_rest_route( 'wca/v1', 'search/(?P<cat>\d+)/(?P<search>.+)/(?P<offset>\d+)', array(
+		'methods' => 'GET',
+		'callback' => 'wca_search_posts',
+		// 'args' => array(
+		// 	'cat' => array(
+		// 		'validate_callback' => is_numeric
+		// 	),
+		// 	'search' => array(
+		// 		'validate_callback' => is_string
+		// 	),
+		// 	'offset' => array(
+		// 		'validate_callback' => is_numeric
+		// 	),
+		// ),
+	));
+	
+	register_rest_route( 'wca/v1', 'pages/(?P<slug>[a-zA-Z0-9-]+)', array(
+		'methods' => 'GET',
+		'callback' => 'wca_pages',
+		// 'args' => array(
+		// 	'slug' => array(
+		// 		'validate_callback' => is_string
+		// 	),
+		// ),
+	));
+});
